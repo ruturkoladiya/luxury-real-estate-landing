bf376c5ae11fbfbfe7a7c2287806231a1ab993a1
Rutu Koladiya <ruturkoladiya@gmail.com>
2026-05-17 22:55:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: [
+      "cpjlcwamma.cloudimg.io",
+      "images.unsplash.com",
+      "source.unsplash.com",
+    ],
+  },
 }
 
 module.exports = nextConfig
